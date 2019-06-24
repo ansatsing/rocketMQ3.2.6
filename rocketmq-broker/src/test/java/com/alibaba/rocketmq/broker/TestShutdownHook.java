dd4395c56f918a4a7098c9abd0c75c60834be986
sunyuanqing <sunyuanqing@ncifangwangluo.com>
2019-06-24 19:45:55 +0800

    shutdownHook测试demo以及钩子执行条件

--- rocketmq-broker/src/test/java/com/alibaba/rocketmq/broker/TestShutdownHook.java
+++ rocketmq-broker/src/test/java/com/alibaba/rocketmq/broker/TestShutdownHook.java
@@ -2,6 +2,14 @@ package com.alibaba.rocketmq.broker;
 
 /**
  * 测试shutdownHook功能:jvm关闭时执行的钩子
+ * 1-程序正常退出
+ * 2-使用System.exit()
+ * 3-终端使用Ctrl+C触发的中断
+ * 4-系统关闭
+ * 5-OutOfMemory宕机
+ * 6-使用Kill pid命令干掉进程（注：在使用kill -9 pid时，是不会被调用的）
+ *
+ *  注意：idea环境debug虚拟机钩子线程需点击Exit按钮
  */
 public class TestShutdownHook {
     public static void main(String[] args) {
